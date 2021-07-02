e9c447a6021eb8214d826c4b8049dbc6fd7612fd
Bismark Atta FRIMPONG <onuapaye@yahoo.com>
2021-07-02 10:47:03 +0200

    changes 1

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require 'simplecov'
-require 'coveralls'
+# require 'coveralls'
 
-Coveralls.wear!
+# Coveralls.wear!
 # SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([SimpleCov::Formatter::HTMLFormatter, Coveralls::SimpleCov::Formatter])
 SimpleCov.start 'rails' do
   add_filter "/spec/"
